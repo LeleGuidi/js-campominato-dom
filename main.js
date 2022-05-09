0d570347bcefebc5f7830051bb85240eb31a6908
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-09 16:43:23 +0200

    Aggiunti commenti

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 //--------------------
 //  FUNCTIONS
 //--------------------
-//Funzione per creare un numero random random
+//Funzione per creare un numero random
 function randomNumbers(min, max) {
     let randomNumber = Math.floor(Math.random() * max) + min;
     return randomNumber;
 }
 
+//Funzione per aggiungere un elemento ad un array solamente se questo non è già presente all'interno dell'array
 function checkNumbersBeforePush(array, number) {
     if (!array.includes(number)) {
         array.push(number)
@@ -20,6 +21,7 @@ function checkNumbersBeforePush(array, number) {
 let userDifficultyChoice
 while (isNaN(userDifficultyChoice) || userDifficultyChoice < 0 || userDifficultyChoice > 2) {
     userDifficultyChoice = Number(prompt(`Inserisci la difficoltà desiderata dallo 0 (più facile) al 2(più difficile)`))
+    //Se l'utente non inserisce correttamente il valore richiesto lo si informa.
     if (isNaN(userDifficultyChoice) || userDifficultyChoice < 0 || userDifficultyChoice > 2) {
         alert(`Devi inserire un NUMERO DA 0 A 2!`)
     }
@@ -35,7 +37,7 @@ if (userDifficultyChoice == 0) {
 } else {
     difficulty = 50;
 }
-//Il computer deve generare 16 numeri casuali compresi da 1 a 100
+//Il computer deve generare 16 numeri casuali compresi da 1 a difficulty (100/80/50)
 let cpuNumbers = [];
 while (cpuNumbers.length < 16) {
     let cpuNumber = randomNumbers(1, difficulty)
@@ -44,15 +46,17 @@ while (cpuNumbers.length < 16) {
 console.log(cpuNumbers)
 
 
-//Chiedere all'utente di inserire un numero per 84 volte, se raggiunge la fine allora ha vinto, se invece ha scelto un numero non corretto... ha perso.
+//Chiedere all'utente di inserire un numero per (difficulty - 16) volte, se raggiunge la fine allora ha vinto, se invece ha scelto un numero non corretto... ha perso.
 let userNumbers = [];
+//Variabile per uscire dal ciclo nel caso l'utente vinca o perda.
 let gameOver = false;
-//Finchè l'utente non ha raggiunto quota 84 numeri inseriti e non ha sbagliato, allora si continua a chiedere il numero.
+//Finchè l'utente non ha raggiunto quota (difficulty - 16) numeri inseriti e non ha sbagliato, allora si continua a chiedere il numero.
 while (gameOver == false) {
-    //Finchè l'utente non inserisce esattamente un numero compreso tra 1 e 100 allora gli lo si richiede.
+    //Finchè l'utente non inserisce esattamente il valore richiesto si ripete la domanda.
     let userNumber
     while (isNaN(userNumber) || userNumber < 1 || userNumber > difficulty) {
         userNumber = Number(prompt(`Inserisci il numero tra 1 a ${difficulty}!`))
+        //Se l'utente non inserisce correttamente il valore richiesto lo si informa.
         if (isNaN(userNumber) || userNumber < 1 || userNumber > difficulty) {
             alert(`Devi inserire un NUMERO DA 1 A ${difficulty}!`)
         }
@@ -68,7 +72,7 @@ while (gameOver == false) {
     //Se il numero è valido, allora viene inserito all'interno della lista utente.
     } else {
         checkNumbersBeforePush(userNumbers, userNumber)
-        //Se l'utente inserisce 84 numeri allora ha vinto!
+        //Se l'utente inserisce (difficulty - 16) numeri allora ha vinto!
         if (userNumbers.length == (difficulty - 17)) {
             gameOver = true;
             alert(`Wow, hai inserito tutti ${userNumbers.length} numeri consentiti! Hai vinto!!`)
