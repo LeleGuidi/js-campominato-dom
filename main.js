048ee507b600b3e5f623ad07316b5cb32a062999
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-09 14:58:09 +0200

    Creata funzione per creare numero random

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+//--------------------
+//  FUNCTIONS
+//--------------------
+//Funzione per creare un numero random random
+function randomNumbers(min, max) {
+    let randomNumber = Math.floor(Math.random() * max) + min;
+    return randomNumber;
+}
+
+
+
+//--------------------
+//  MAIN
+//--------------------
 //Il computer deve generare 16 numeri casuali compresi da 1 a 100
 let cpuNumbers = [];
 while (cpuNumbers.length < 16) {
-    let number = Math.floor(Math.random() * 100) + 1;
+    let number = randomNumbers(1, 100)
     if (!cpuNumbers.includes(number)) {
         cpuNumbers.push(number)
     }
